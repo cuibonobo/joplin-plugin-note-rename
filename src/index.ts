27cf3358eaa60d939d728c3e49fb617ba17eae41
Jen Garcia <jen@haverstack.com>
2023-01-23 19:26:46 -0500

    Basic functionality and menu items

--- src/index.ts
+++ src/index.ts
@@ -5,46 +5,104 @@ joplin.plugins.register({
 	onStart: async function() {
 		console.info('Note Rename plugin started!');
 
-		const addPrefix = async (prefix: string) => {
+		const prefixNoteNames = async (prefix: string) => {
+			await renameSelected((noteName: string) => {
+				return prefix + noteName;
+			});
+		};
+
+		const suffixNoteNames = async (suffix: string) => {
+			await renameSelected((noteName: string) => {
+				return noteName + suffix;
+			});
+		};
+
+		const searchAndReplaceNoteNames = async (search: string, replace: string) => {
+			await renameSelected((noteName: string) => {
+				const escapedSearch = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+				return noteName.replace(new RegExp(escapedSearch, "g"), replace);
+			});
+		};
+
+		const renameSelected = async (renameFn: (noteName: string) => string) => {
 			const noteIds = await joplin.workspace.selectedNoteIds();
 			const notes: any[] = [];
 			for (let noteId of noteIds) {
-				console.info(noteId);
 				notes.push(await joplin.data.get(["notes", noteId]))
 			}
 			// FIXME: Reversing order of notes to preserve original modified order,
 			// but we should look at the actual dates of the notes instead
 			notes.reverse();
 			for (let note of notes) {
-				await joplin.data.put(["notes", note.id], null, {title: prefix + note.title})
+				await joplin.data.put(["notes", note.id], null, {title: renameFn(note.title)})
 			}
 		};
 
-		const handle = await joplin.views.dialogs.create('noteRenamePrefix');
-		await joplin.views.dialogs.setHtml(handle, `
-		<h2>Note Rename: Prefix</h2>
-		<form name="prefix">
-		Prefix: <input type="text" name="input"/>
+		const alterHandle = await joplin.views.dialogs.create('noteNameAlter');
+		await joplin.views.dialogs.setHtml(alterHandle, `
+		<h4>Note Rename: Prefix/Suffix</h4>
+		<form name="rename">
+			<fieldset style="margin-bottom: 1rem;">
+					<legend>Select a mode:</legend>
+					<input type="radio" id="prefix" name="mode" value="prefix" checked>
+					<label for="prefix">Prefix</label>
+					<br />
+					<input type="radio" id="suffix" name="mode" value="suffix">
+					<label for="suffix">Suffix</label>
+			</fieldset>
+			Text: <input type="text" name="input" style="margin-bottom: 1rem;" />
+		</form>
+		`);
+
+		const replaceHandle = await joplin.views.dialogs.create('noteNameReplace');
+		await joplin.views.dialogs.setHtml(replaceHandle, `
+		<h4>Note Rename: Search and Replace</h4>
+		<form name="replace">
+			Search: <input type="text" name="search" style="margin-bottom: 1rem;" />
+			Replace: <input type="text" name="replace" style="margin-bottom: 1rem;" />
 		</form>
 		`);
 
 		await joplin.commands.register({
-			name: 'noteRenamePrefix',
-			label: "Note rename: Prefix",
-			iconName: "fas fa-music",
+			name: 'noteNameAlter',
+			label: "Prefix / Suffix",
 			execute: async () => {
-				console.info("Executing prefix command");
-				const result = await joplin.views.dialogs.open(handle);
-				// {"id":"ok","formData":{"prefix":{"input":"foo"}}}
+				console.info("Note Rename: Executing alter command");
+				const result = await joplin.views.dialogs.open(alterHandle);
 				if (result["id"] != "ok") {
 					return;
 				}
-				await addPrefix(result["formData"]["prefix"]["input"]);
+				const formData = result["formData"]["rename"];
+				if (formData["mode"] == "suffix") {
+					await suffixNoteNames(formData["input"]);
+				} else {
+					await prefixNoteNames(formData["input"]);
+				}
 			}
 		});
 
-		await joplin.views.toolbarButtons.create('noteRenameButton1', 'noteRenamePrefix', ToolbarButtonLocation.NoteToolbar);
-		await joplin.views.menuItems.create('noteRenameContextItem1', 'noteRenamePrefix', MenuItemLocation.NoteListContextMenu);
-		await joplin.views.menuItems.create('noteRenameMenuItem1', 'noteRenamePrefix', MenuItemLocation.Tools);
+		await joplin.commands.register({
+			name: 'noteNameReplace',
+			label: "Search and Replace",
+			execute: async () => {
+				console.info("Note Rename: Executing replace command");
+				const result = await joplin.views.dialogs.open(replaceHandle);
+				if (result["id"] != "ok") {
+					return;
+				}
+				const formData = result["formData"]["replace"];
+				await searchAndReplaceNoteNames(formData["search"], formData["replace"]);
+			}
+		});
+
+		await joplin.views.toolbarButtons.create('noteRenameAlterButton', 'noteNameAlter', ToolbarButtonLocation.NoteToolbar);
+		await joplin.views.toolbarButtons.create('noteRenameReplaceButton', 'noteNameReplace', ToolbarButtonLocation.NoteToolbar);
+
+		// FIXME: Creating a menu instead of a menuItem at NoteListContextMenu won't work and breaks toolbar buttons
+		await joplin.views.menuItems.create('noteRenameAlterContextItem', 'noteNameAlter', MenuItemLocation.NoteListContextMenu);
+		await joplin.views.menuItems.create('noteRenameReplaceContextItem', 'noteNameReplace', MenuItemLocation.NoteListContextMenu);
+
+		const menuItems = [{commandName: 'noteNameAlter'}, {commandName: 'noteNameReplace'}];
+		await joplin.views.menus.create('noteRenameToolsMenu', 'Note Rename', menuItems);
 	},
 });
